9f7e7599cc74b2f1b818ca3fcd35631bfb864760
Vishal Garg <vishugarg10004@gmail.com>
2020-10-24 15:41:01 +0530

    [Vishal] Welcome Message

---
+++ LinkedListMSTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinkedListMSTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
